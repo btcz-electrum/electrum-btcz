c42af301543d89585a26079fa5ac3adee7a28062
cryptorex <hungryhodlr@scryptmail.com>
2021-12-29 13:23:48 -0600

    add checkpoints; version bump

--- lib/version.py
+++ lib/version.py
@@ -1,4 +1,4 @@
-ELECTRUM_VERSION = '3.1.3b5'   # version of the client package
+ELECTRUM_VERSION = '3.1.3b6'   # version of the client package
 PROTOCOL_VERSION = '1.4'     # protocol version requested
 
 # The hash of the mnemonic seed must begin with this
